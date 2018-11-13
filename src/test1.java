85a4cfa9c5078d69c2826b4854d48034c1108e7f
Albin <Albin644@gmail.com>
2018-11-13 21:54:13 +0100

    Added some comments

--- src/test1.java
+++ src/test1.java
@@ -9,40 +9,55 @@ public class test1 {
     private String employee = "name";
     private String employee2 = "name2";
 
+    /**
+     * When starttime < 0 addWorkingPeriod returns false
+     */
     @Test
     public void addWorkingPeriod_Test1_part1(){
         int startTime = -5;
         int endTime = 5;
-        //WorkSchedule.Hour scheduleBefore = workschedule.readSchedule(5);
 
         assertFalse(workschedule.addWorkingPeriod(employee, startTime, endTime));
-        //WorkSchedule.Hour scheduleAfter = workschedule.readSchedule(5);
         //Add an assert to check that the schedule is unchanged.
     }
 
+    /**
+     * When endtime >= size(size of the schedule) addWorkingPeriod returns false
+     */
     @Test
     public void addWorkingPeriod_Test1_part2(){
-        int startTime = 18;
-        int endTime = 15;
-
-        assertFalse(workschedule.addWorkingPeriod(employee, startTime, endTime));
-    }
-
-    @Test
-    public void addWorkingPeriod_Test1_part3(){
         int startTime = 18;
         int endTime = 26;
 
         assertFalse(workschedule.addWorkingPeriod(employee, startTime, endTime));
     }
 
+    /**
+     * When starttime > endtime addWorkingPeriod should return false
+     */
+    @Test
+    public void addWorkingPeriod_Test1_part3(){
+        int startTime = 18;
+        int endTime = 15;
+
+        assertFalse(workschedule.addWorkingPeriod(employee, startTime, endTime));
+    }
+
+    /**
+     * When the required number for an hour in the interval starttime-endtime is already met, addWorkingPeriod returns false
+     * and schedule is unchanged
+     */
     @Test
     public void addWorkingPeriod_Test1_part4(){
         int startTime = 5;
         int endTime = 10;
+        int requiredNumber = 0;
 
-        workschedule.setRequiredNumber(0, startTime, endTime);
-        assertFalse(workschedule.addWorkingPeriod(employee, startTime, endTime));
+        workschedule.setRequiredNumber(requiredNumber, startTime, endTime);
+
+        assertTrue(workschedule.workingEmployees(startTime, endTime).length == 0);//No employees in the schedule
+        assertFalse(workschedule.addWorkingPeriod(employee, startTime, endTime)); //Returns false and schedule is unchanged
+        assertTrue(workschedule.workingEmployees(startTime, endTime).length == 0); //No employee was added
     }
 
     @Test
@@ -51,9 +66,9 @@ public class test1 {
         int endTime = 10;
 
         workschedule.setRequiredNumber(2, startTime, endTime);
-        assertTrue(workschedule.addWorkingPeriod(employee, startTime, endTime));
-        assertFalse(workschedule.addWorkingPeriod(employee, startTime, endTime));
-        assertTrue(workschedule.addWorkingPeriod(employee2, startTime, endTime));
+        assertTrue(workschedule.addWorkingPeriod(employee, startTime, endTime));//The method returns true and employee is added
+        assertFalse(workschedule.addWorkingPeriod(employee, startTime, endTime));//The method returns false, employee is already added
+        assertTrue(workschedule.addWorkingPeriod(employee2, startTime, endTime));////The method returns true and employee2 is added
     }
 
 }
