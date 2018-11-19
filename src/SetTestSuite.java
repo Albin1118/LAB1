88f7928d52cfa0737fd2946dd3c271662148275b
Albin <Albin644@gmail.com>
2018-11-19 19:37:01 +0100

    Added TestSuites

---
+++ src/SetTestSuite.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        Set_Test_insert.class,
+        Set_Test_member.class,
+        Set_Test_section.class,
+        Set_Test_containsArithTriple.class
+        })
+public class SetTestSuite {
+    // nothing goes here
+}
---
+++ src/WorkScheduleTestSuite.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        WorkSchedule_Test1.class,
+        WorkSchedule_Test2.class
+        })
+public class WorkScheduleTestSuite {
+    // nothing goes here
+}
